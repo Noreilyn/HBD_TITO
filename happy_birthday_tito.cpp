d0f9026372a7b07d9676c74100e9061cb2f15ddc
Noreilyn <151014013+Noreilyn@users.noreply.github.com>
2024-05-18 00:57:15 +0800

    Add files via upload

---
+++ happy_birthday_tito.cpp
@@ -0,0 +1,64 @@
+
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h> 
+using namespace std;
+
+int main()
+{
+ int i;
+ char h=3;
+ system("cls");
+ system("color f5"); 
+ cout<<"\t\t\tFrom: Kenshin <<<333\n";
+ Sleep(1000);
+ cout<<"\t\t\tTo  : Tito Emer\n\n";
+ Sleep(1000);
+ cout<<"\t\t";
+ for(i=0;i<42;i++)
+ cout<<"_";
+ cout<<"\n\n";
+ system("color 0B"); 
+
+ Sleep(1000);
+ cout<<"\t\t\t\ti i i i i\n";
+ 
+ Sleep(1000);
+ cout<<"\t\t\t\ti i i i i\n";
+ 
+ Sleep(1000);
+ cout<<"\t\t\t      __i_i_i_i_i__\n";
+ 
+ Sleep(1000);
+ cout<<"\t\t\t     |"<<h<<"           "<<h<<"|\n";
+ 
+ Sleep(1000);
+ cout<<"\t\t\t   __| "<<h<<"  17/05  "<<h<<" |__\n";
+ 
+ Sleep(1000);
+ cout<<"\t\t\t  | "<<h<<"   "<<h<<"       "<<h<<"   "<<h<<" |\n";
+
+ Sleep(1000);
+ cout<<"\t\t\t__|"<<h<<"   HAPPY     "<<h<<"   "<<h<<"|__\n";
+
+ Sleep(1000);
+ cout<<"\t\t       | "<<h<<"   "<<h<<"     BIRTHDAY    "<<h<<" |\n";
+
+ Sleep(1000);
+ cout<<"\t\t       |"<<h<<"  "<<h<<"     "<<h<<"    "<<h<<"     "<<h<<"   "<<h<<"|\n"; 
+ 
+ 
+ Sleep(1000);
+ cout<<"\t\t       | "<<h<<" "<<" "<<h<<" "<<h<<"  T I T O  "<<h<<" "<<h<<" "<<" "<<h<<" |\n";
+
+ Sleep(1000);
+ cout<<"\t\t       | "<<h<<" "<<" "<<h<<" "<<h<<"  E M E R  "<<h<<" "<<h<<" "<<" "<<h<<" |\n";
+    
+ Sleep(1000);
+ cout<<"\t\t       |_________________________|\n";    
+    
+ cout<<"\n\n\n\n\n"; 
+ system("pause");
+ 
+ return 0;
+}
